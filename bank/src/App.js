63aa8191e009ba7357bf750c5f4ad59c53d67e2f
Your Name <you@example.com>
2019-12-02 17:50:04 +0200

    Bank front and back - complete

--- bank/src/App.js
+++ bank/src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Transactions from './components/Transactions';
+import Operations from './components/Operations';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      transactions: [
+        { amount: 3200, vendor: "Elevation", category: "Salary" },
+        { amount: -7, vendor: "Runescape", category: "Entertainment" },
+        { amount: -20, vendor: "Subway", category: "Food" },
+        { amount: -98, vendor: "La Baguetterie", category: "Food" }
+      ],
+
+      newTransaction: {
+        amount: "",
+        vendor: "",
+        category: ""
+      }
+
+    }
+  }
+
+  balance = () => {
+    let balance = 0
+    console.log(this.state)
+    this.state.transactions.forEach(t => balance += t.amount)
+    return balance
+  }
+
+  withdraw = async(amount, vendor, category) => {
+    let transaction = { amount: (-amount), vendor: vendor, category: category }
+      let transactions = await axios.post('http://localhost:1309/transactions', {data:{transaction}})
+      this.setState({ transactions: transactions.data })
+    
+  }
+
+  deposit = async(amount, vendor, category) => {
+    let newTransaction = { amount: amount, vendor: vendor, category: category }
+      let transactions = await axios.post('http://localhost:1309/transactions', {transaction: newTransaction})
+      this.setState({ transactions: transactions.data })
+  }
+
+   updateNewTransection = async(e) => {
+    let value = e.target.value
+    let name = e.target.name
+    let newTransaction = this.state.newTransaction
+    newTransaction[name] = value
+    this.setState({newTransaction})
+  }
+
+
+
+   removeTransaction = async(id) =>{
+    let transactions = await axios.delete('http://localhost:1309/transactions', {data:{id:id}} )
+    this.setState({transactions: transactions.data})
+  }
+    
+
+  async updateTransections() {
+    return axios.get('http://localhost:1309/transactions')
+  }
+
+
+  async componentDidMount() {
+    let transactions = await this.updateTransections()
+    this.setState({ transactions: transactions.data })
+  }
+
+
+  render() {
+
+    return (this.state.transactions ? 
+      <div id="main-container">
+        <div id="sum">Total amount: {this.balance()}</div>
+        <div id="new-transaction-details">
+          <div id="inputs">
+            <input type="text" name="amount" placeholder="insert amount" onChange={this.updateNewTransection}></input>
+            <input type="text" name="vendor" placeholder="insert vendor" onChange={this.updateNewTransection}></input>
+            <input type="text" name="category" placeholder="insert category" onChange={this.updateNewTransection}></input>
+          </div>
+          <Operations withdraw={this.withdraw} deposit={this.deposit} amount={this.state.newTransaction.amount} vendor={this.state.newTransaction.vendor} category={this.state.newTransaction.category} />
+        </div>
+
+        <Transactions transData={this.state.transactions} removeTransaction={this.removeTransaction} />
+      </div>
+    : null) 
+  }
 }
+
 
 export default App;
