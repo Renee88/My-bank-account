3cd7ab3446c01324a9c23ee49869c1a7097f9bd6
Your Name <you@example.com>
2019-12-06 11:24:17 +0200

    display is empty when there are no transactions

--- bank/src/components/Breakdown.jsx
+++ bank/src/components/Breakdown.jsx
@@ -7,12 +7,11 @@ class Breakdown extends Component {
 
     render() {
         let transactions = this.props.transactions
-        let groups = Object.keys(this.props.balance)
         return (
             <div id="breakdown-container">
                 <SelectMonth />
-                <div id="breakdown" >
-                    <div className="group title"><div>Group</div> <div>Total</div></div>
+                {transactions.length  ? <div id="breakdown" >
+                 <div className="group title"><div>Group</div> <div>Total</div></div> 
                     {this.props.balance.map(g => {
                         return <div className={g.amount > 0 ? "group positive transaction" : "group negative transaction"}>
                             <div className={g._id}>{g._id}</div>
@@ -22,12 +21,7 @@ class Breakdown extends Component {
                             </div>
                         </div>
                     })}
-                    {/* <div className = "modal-container">
-                    {transactions.map(t => {
-                            return <div className="modal">{t.date} {t.vendor} {t.category} {t.amount}</div>
-                    })}
-                    </div> */}
-                </div>
+                </div> :null}
             </div>
 
         )
--- bank/src/components/Transactions.jsx
+++ bank/src/components/Transactions.jsx
@@ -6,10 +6,10 @@ class Transactions extends Component {
     render() {
         console.log(this.props.transData)
         return (
-            <div id="transactions">
-                <div className = "transaction title"><div>Date</div><div>Vendor</div> <div>Group</div><div>{'Amount($)'}</div></div>
-                {this.props.transData.map(t => <Transaction key = {t._id}  singleTransData={t} id={t._id} removeTransaction={this.props.removeTransaction} />)}
-            </div>
+            this.props.transData.length ? <div id="transactions">
+                <div className="transaction title"><div>Date</div><div>Vendor</div> <div>Group</div><div>{'Amount($)'}</div></div>
+                {this.props.transData.map(t => <Transaction key={t._id} singleTransData={t} id={t._id} removeTransaction={this.props.removeTransaction} />)}
+            </div> : null
         );
     }
 }
