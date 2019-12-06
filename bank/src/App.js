8c02ee6394d21bd1b82770702967be0425e331ec
Your Name <you@example.com>
2019-12-06 18:36:33 +0200

    error handling and improved effects for buttons

--- bank/src/App.js
+++ bank/src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
   }
 
   firstToUpperCase = (word) => {
-    return word[0].toUpperCase() + word.slice(1).toLowerCase()
+    return word ? word[0].toUpperCase() + word.slice(1).toLowerCase() : null
   }
 
   breakdown = async () => {
