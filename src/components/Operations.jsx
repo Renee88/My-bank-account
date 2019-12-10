969c1c533cf1029b4b04c01baaf6cd1a45a9e352
Your Name <you@example.com>
2019-12-10 10:00:41 +0200

    snackbars moved back to a separate file

--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -1,44 +1,10 @@
 import React, { Component } from 'react';
 import DatePickerDialog from './DatePickerDialog';
-import { Button, Snackbar} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import Snackbars from './Snackbars'
 
-
-function TransitionUp(props) {
-    return <Slide {...props} direction="up" />;
-}
-
-const useStyles = makeStyles(theme => ({
-    close: {
-        padding: theme.spacing(0.5),
-    },
-}));
 
 
 function Operations(props) {
-
-    const classes = useStyles();
-    const [transition, setTransition] = React.useState(undefined);
-    const [open, setOpen] = React.useState(false);
-
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-
-    const handleClick = Transition => () => {
-        setTransition(() => Transition);
-        setOpen(true);
-    };
-
 
 
     const withdraw = () => {
@@ -63,30 +29,7 @@ function Operations(props) {
 
             <div id="date"><DatePickerDialog updateDate={props.updateDate} /></div>
 
-            <div id="buttons">
-
-            </div>
-            <Button onClick={handleClick(TransitionUp)}> <div id="deposit" className="button" onClick={deposit}>Deposit</div> </Button>
-            <Button onClick={handleClick(TransitionUp)}> <div id="withdraw" className="button" onClick={withdraw}>Withdraw</div> </Button>
-
-            <Snackbar
-                open={open}
-                autoHideDuration={3000}
-                onClose={handleClose}
-                ContentProps={{
-                    'aria-describedby': 'message-id',
-                }}
-                message={<span id="message-id"> {props.didUpdate ? "Successfully updated" : "Update failed. Please try again"} </span>}
-                action={[<IconButton
-                    key="close"
-                    aria-label="close"
-                    color="inherit"
-                    className={classes.close}
-                    onClick={handleClose}
-                ><CloseIcon />
-                </IconButton>
-                ]}
-            />
+            <Snackbars deposit = {deposit} withdraw ={withdraw} />
         </div>
     );
 
--- src/components/Snackbars.jsx
+++ src/components/Snackbars.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import {Snackbar} from '@material-ui/core';
+import {Button, Snackbar} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
+const useStyles = makeStyles(theme => ({
+  close: {
+    padding: theme.spacing(0.5),
+  },
+}));
 
 
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
 
 export default function Snackbars(props) {
 
+  const classes = useStyles();
+  const [transition, setTransition] = React.useState(undefined);
+  const [open, setOpen] = React.useState(false);
 
-  return (  
-    <div className = "snackbar">
+  const handleClick = Transition => () => {
+    setTransition(() => Transition);
+    setOpen(true);
+  };
+
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div id="buttons">
+      <Button onClick={handleClick(TransitionUp)}> <div id="deposit" className = "button" onClick={props.deposit}>Deposit</div> </Button>
+      <Button onClick={handleClick(TransitionUp)}> <div id="withdraw" className = "button" onClick={props.withdraw}>Withdraw</div> </Button>
       <Snackbar
          open={open}
          autoHideDuration={3000}
