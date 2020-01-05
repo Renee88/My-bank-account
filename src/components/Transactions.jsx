e705b4980f99dddf2c87d658bd973fe836a94090
Your Name <you@example.com>
2020-01-05 19:43:06 +0200

    images update

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -8,7 +8,7 @@ class Transactions extends Component {
         return (
             this.props.transData.length ? <div id="transactions">
                 <table>
-                <tr className="transaction title"><th className = "title-col">Date</th><th className ="title-col">Vendor</th> <th className = "title-col">Group</th> <th className = "title-col">{'Amount($)'}</th></tr>
+                <tr className="transaction title"><th className = "title-col">Date</th><th className ="title-col">Vendor</th> <th className = "title-col">Category</th> <th className = "title-col">{'Amount($)'}</th></tr>
                 {this.props.transData.map(t => <Transaction key={t._id} singleTransData={t} id={t._id} firstToUpperCase = {this.props.firstToUpperCase} removeTransaction={this.props.removeTransaction} />)}
                 </table>
             </div> : <p className = "nothing-to-display">No transactions to display</p>
